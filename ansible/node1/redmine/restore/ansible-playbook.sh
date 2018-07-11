f39f6751f6205919be2b1e0af94ac84931c2b35f
Tomonori Matsumura <matsumura_t@yahoo.co.jp>
2018-07-12 01:02:51 +0900

    job「ansible/topic/node1/redmine/restore」でcredentialsを利用して処理するように変更した refs #568

--- ansible/node1/redmine/restore/ansible-playbook.sh
+++ ansible/node1/redmine/restore/ansible-playbook.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-expect -c " 
-    set timeout -1  
+expect -c "
+    set timeout -1
     spawn bash -c \"ansible-playbook /WORKDIR/redmine_restore.yml \
     --become-method=su \
     --ask-become-pass \
@@ -10,10 +10,10 @@ expect -c "
     -e "db_password=DBPASSWORD" \
     -e "backup_data_host=BACKUPDATAHOST" \
     | tee /WORKDIR/.ansible.log\"
- 
+
     expect SU*
     send -- \"SUPASSWORD\n\"
- 
+
     expect eof
     exit
     "
